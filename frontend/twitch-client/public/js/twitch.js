0c445bd431017b9fa0fdc996627c13d41c1fd184
Peter Martinson <peter.j.martinson@gmail.com>
2017-05-23 19:21:37 -0400

    Add qunit

--- frontend/twitch-client/public/js/twitch.js
+++ frontend/twitch-client/public/js/twitch.js
@@ -1,9 +1,9 @@
 var twitch = {};
 
 twitch.fetchJSON = function() {
-  // $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-  //   console.log(data);
-  // });
+  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+    console.log(data);
+  });
   return true;
 }
 
